3698090dfd4433e3748c159afc34ffe311b429c2
jake-drysdale <drysdalejake95@gmail.com>
2024-03-27 15:23:23 +0000

    updated ab test generator to use references if needed, added similarity evaluations

--- python/ab_test_generator.py
+++ python/ab_test_generator.py
@@ -23,11 +23,11 @@ def load_and_append_xml(xml_path, additional_xml):
     # Return the modified XML tree
     return tree
 
-def generate_test_pages(num_pages):
+def generate_test_pages(num_pages, audio_path, outside_ref=False):
     xml = '<root>'  # Add a root element
     for i in range(num_pages):
-        xml += f'<page id="test-{i}" hostURL="eval_audio/" randomiseOrder="true" repeatCount="0" loop="false"\n'
-        xml += '    synchronous="false" loudness="-12" label="default" labelStart="" poolSize="0"\n'
+        xml += f'<page id="test-{i}" hostURL="{audio_path}" randomiseOrder="true" repeatCount="0" loop="false"\n'
+        xml += '    synchronous="false" loudness="0" label="default" labelStart="" poolSize="0"\n'
         xml += '    alwaysInclude="false" preSilence="0" postSilence="0" playOne="false"\n'
         xml += '    restrictMovement="false" minNumberPlays="0">\n'
         xml += '    <commentboxprefix>Comment on track</commentboxprefix>\n'
@@ -36,6 +36,8 @@ def generate_test_pages(num_pages):
         xml += '        alwaysInclude="false" preSilence="0" postSilence="0" minNumberPlays="0" />\n'
         xml += f'    <audioelement url="{i}_B.wav" id="track-{2*i+1}" gain="0" label="" type="normal"\n'
         xml += '        alwaysInclude="false" preSilence="0" postSilence="0" minNumberPlays="0" />\n'
+        if outside_ref:
+            xml += f'    <audioelement url="{i}_X.wav" id="ref-{2*i}" gain="0" type="outside-reference" />\n'
         xml += '    <survey location="pre" showBackButton="true" />\n'
         xml += '    <survey location="post" showBackButton="true" />\n'
         xml += '</page>\n'
@@ -47,6 +49,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     print(os.getcwd())
     parser.add_argument("--xml_path", default="./python/base_test_template.xml", help="Path to the XML file (default: %(default)s)")
+    parser.add_argument("--audio_path", default="eval_audio/", help="Path to audio files to be evaluated (default: %(default)s)")
+    parser.add_argument("--ref", default=False, help="Whether or not to include reference audio as part of the test (e.g., 1_X.wav)")
     parser.add_argument("--num_pages", type=int, required=True, help="Number of additional pages to add")
     parser.add_argument("--test_title", type=str, required=True, help="Name of evaluation experiment")
     args = parser.parse_args()
@@ -54,7 +58,7 @@ if __name__ == "__main__":
     # args.num_pages = 20
     # args.test_title = "beatoven_ab_template"
 
-    test_pages = generate_test_pages(args.num_pages)
+    test_pages = generate_test_pages(args.num_pages, args.audio_path, args.ref)
     # Load base xml and append test pages
     updated_test_xml = load_and_append_xml(args.xml_path, test_pages)
 
